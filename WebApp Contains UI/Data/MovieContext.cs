5d33f6e3502660155792846adb9f275b81927755
허준도 <jundo123@live.co.kr>
2020-09-15 17:20:28 +0900

    여러가지 추가

---
+++ WebApp Contains UI/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Contains_UI.Models;
+
+namespace WebApp_Contains_UI.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp_Contains_UI.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebApp Contains UI/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Contains_UI.Data;
+using WebApp_Contains_UI.Models;
+
+namespace WebApp_Contains_UI.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApp_Contains_UI.Data.MovieContext _context;
+
+        public IndexModel(WebApp_Contains_UI.Data.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
